9eab7357fec70945ee58051c19087688f9123d1f
Yasiru Kaveeshwara <yasiruk@visionexdigital.com.au>
2025-12-05 12:28:51 +0530

    setup backend

--- backend/database/migrations/2025_12_04_161333_create_products_table.php
+++ backend/database/migrations/2025_12_04_161333_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('image_path');
+            $table->string('original_image_path')->nullable();
             $table->timestamps();
         });
     }
